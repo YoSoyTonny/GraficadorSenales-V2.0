443a9667b04966c5e3bfaa93a6824052a3e0e7ef
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2018-10-31 11:49:09 -0700

    Se agrego un espacio para una señal rectangular

---
+++ GraficadorSeñales/SeñalRectangular.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalRectangular : Señal
+    {
+        public SeñalRectangular()
+        {
+            Alpha = 1.0;
+            Umbral = 0.0;
+            Muestras = new List<Muestra>();
+            AmplitudMaxima = 0.0;
+        }
+
+    }
+}
